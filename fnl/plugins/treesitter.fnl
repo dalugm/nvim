606548ac954248e4f548e285731c49230940580f
dalu <mou.tong@outlook.com>
2024-05-25 14:18:09 +0800

    Use fennel instead of lua

---
+++ fnl/plugins/treesitter.fnl
@@ -0,0 +1,19 @@
+[{1 :nvim-treesitter/nvim-treesitter
+  :build ":TSUpdate"
+  :config (fn []
+            (let [treesitter (require :nvim-treesitter.configs)]
+              (treesitter.setup {:highlight {:enable true}
+                                 :indent {:enable true}
+                                 :ensure_installed [:bash
+                                                    :clojure
+                                                    :commonlisp
+                                                    :fennel
+                                                    :html
+                                                    :java
+                                                    :javascript
+                                                    :json
+                                                    :lua
+                                                    :markdown
+                                                    :rust
+                                                    :toml
+                                                    :yaml]})))}]
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,59 +1,14 @@
--- Install package management.
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-   vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable", -- latest stable release
-      lazypath,
-   })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to set `mapleader` before lazy so mappings are correct.
-vim.g.mapleader = " "
-
-require("lazy").setup({
-   "tpope/vim-repeat",
-   "tpope/vim-rsi",
-   "tpope/vim-surround",
-   "tpope/vim-unimpaired",
-
-   -- Colorscheme --
-   "dalugm/solarized.vim",
-   "morhetz/gruvbox",
-   -- Alias plugin names.
-   { "dracula/vim", name = "dracula" },
-
-   {
-      "numToStr/Comment.nvim",
-      config = function()
-         require("Comment").setup()
-      end,
-   },
-
-   {
-      "windwp/nvim-autopairs",
-      config = function()
-         require("nvim-autopairs").setup()
-      end,
-   },
-
-   {
-      "lewis6991/gitsigns.nvim",
-      config = function()
-         require("gitsigns").setup({
-            signs = {
-               add = { text = "+" },
-               change = { text = "=" },
-               delete = { text = "-" },
-               topdelete = { text = "‾" },
-               changedelete = { text = "~" },
-               untracked = { text = "?" },
-            },
-         })
-      end,
-   },
-})
+return {
+  {
+    "folke/lazy.nvim",
+    version = "*"
+  },
+  {
+    "Olical/nfnl",
+    ft = "fennel",
+    dependencies = { "norcalli/nvim.lua" },
+    init = function()
+      require("config")
+    end,
+  },
+}
